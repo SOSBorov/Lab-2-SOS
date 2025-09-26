3331089f9e1a99b2c7ef777e13044d31f8b47e56
Борис <egorhasdef@gmail.com>
2025-09-26 20:57:32 +0300

    todolist oda

---
+++ TodoList/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+namespace OMGKERDICK
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Работу выполнили Vasilevich и Garmash");
+
+			Console.WriteLine("Продиктуйте ваше имя и фамилию мессир: ");
+			string fullname = Console.ReadLine();
+
+			Console.WriteLine("Продиктуйте ваш год рождения: ");
+			DateTime birthdayDate = DateTime.ParseExact(Console.ReadLine(), "yyyy", CultureInfo.InvariantCulture);
+			DateTime currentDate = DateTime.Today;
+			int age = currentDate.Year - birthdayDate.Year;
+
+			Console.WriteLine(" Добавлен пользователь " + fullname + ", " + "возраст - " + age);
+			Console.ReadKey();
+		}
+	}
+}
