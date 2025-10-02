c915a7edcd9c8e6897178ea1b3aaeb64ddfb3978
Борис <egorhasdef@gmail.com>
2025-10-02 18:50:00 +0300

    lesson 3 make 2

--- TodoList/Program.cs
+++ TodoList/Program.cs
@@ -1,22 +1,27 @@
 ﻿using System.Globalization;
-namespace OMGKERDICK
+namespace lab2
 {
-	internal class Program
-	{
-		static void Main(string[] args)
-		{
-			Console.WriteLine("Работу выполнили Vasilevich и Garmash");
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Работу выполнили Vasilevich и Garmash");
 
-			Console.WriteLine("Продиктуйте ваше имя и фамилию мессир: ");
-			string fullname = Console.ReadLine();
+            Console.WriteLine("Продиктуйте ваше имя и фамилию мессир: ");
+            string fullname = Console.ReadLine();
 
-			Console.WriteLine("Продиктуйте ваш год рождения: ");
-			DateTime birthdayDate = DateTime.ParseExact(Console.ReadLine(), "yyyy", CultureInfo.InvariantCulture);
-			DateTime currentDate = DateTime.Today;
-			int age = currentDate.Year - birthdayDate.Year;
+            Console.WriteLine("Продиктуйте ваш год рождения: ");
+            DateTime birthdayDate = DateTime.ParseExact(Console.ReadLine(), "yyyy", CultureInfo.InvariantCulture);
+            DateTime currentDate = DateTime.Today;
+            int age = currentDate.Year - birthdayDate.Year;
 
-			Console.WriteLine(" Добавлен пользователь " + fullname + ", " + "возраст - " + age);
-			Console.ReadKey();
-		}
-	}
+            Console.WriteLine(" Добавлен пользователь " + fullname + ", " + "возраст - " + age);
+
+            static void second(string[] args)
+            {
+                string[] todos;
+
+            }
+        }
+    }
 }
