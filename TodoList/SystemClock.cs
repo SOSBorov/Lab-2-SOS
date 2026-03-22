54bbd5f9a74bb272cffd23ac6a029f16a888fe28
Борис <egorhasdef@gmail.com>
2026-03-22 16:40:03 +0300

    lesson 3: create SystemClock

---
+++ TodoList/SystemClock.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TodoList.Interfaces;
+
+namespace TodoList;
+
+public class SystemClock : IClock
+{
+	public DateTime Now => DateTime.Now;
+}
