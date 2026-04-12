3effd10d7655e01eaa50098a46b475beea9e6dc2
Борис <egorhasdef@gmail.com>
2026-04-12 19:28:15 +0300

    lesson 6: make Repositoriess dda

---
+++ TodoList/Services/ProfileRepository.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using TodoList.Exceptions;
+
+namespace TodoList
+{
+	public class ProfileRepository
+	{
+		public List<Profile> GetAll()
+		{
+			using var context = new AppDbContext();
+			context.Database.EnsureCreated();
+
+			return context.Profiles
+				.AsNoTracking()
+				.Include(profile => profile.TodoItems)
+				.OrderBy(profile => profile.Login)
+				.ToList();
+		}
+
+		public Profile? GetById(Guid id)
+		{
+			using var context = new AppDbContext();
+			context.Database.EnsureCreated();
+
+			return context.Profiles
+				.AsNoTracking()
+				.Include(profile => profile.TodoItems)
+				.FirstOrDefault(profile => profile.Id == id);
+		}
+
+		public Profile? GetByLogin(string login)
+		{
+			using var context = new AppDbContext();
+			context.Database.EnsureCreated();
+
+			return context.Profiles
+				.AsNoTracking()
+				.Include(profile => profile.TodoItems)
+				.FirstOrDefault(profile => profile.Login == login);
+		}
+
+		public Profile? GetByCredentials(string login, string password)
+		{
+			using var context = new AppDbContext();
+			context.Database.EnsureCreated();
+
+			return context.Profiles
+				.AsNoTracking()
+				.Include(profile => profile.TodoItems)
+				.FirstOrDefault(profile => profile.Login == login && profile.Password == password);
+		}
+
+		public void Add(Profile profile)
+		{
+			using var context = new AppDbContext();
+			context.Database.EnsureCreated();
+
+			bool loginExists = context.Profiles.Any(existingProfile => existingProfile.Login == profile.Login);
+			if (loginExists)
+			{
+				throw new DuplicateLoginException("Этот логин уже занят.");
+			}
+
+			context.Profiles.Add(profile);
+			context.SaveChanges();
+		}
+
+		public void Update(Profile profile)
+		{
+			using var context = new AppDbContext();
+			context.Database.EnsureCreated();
+
+			var existingProfile = context.Profiles.FirstOrDefault(item => item.Id == profile.Id);
+			if (existingProfile == null)
+			{
+				throw new ProfileNotFoundException($"Профиль с ID '{profile.Id}' не найден.");
+			}
+
+			bool loginExists = context.Profiles.Any(item => item.Id != profile.Id && item.Login == profile.Login);
+			if (loginExists)
+			{
+				throw new DuplicateLoginException("Этот логин уже занят.");
+			}
+
+			existingProfile.Login = profile.Login;
+			existingProfile.Password = profile.Password;
+			existingProfile.FirstName = profile.FirstName;
+			existingProfile.LastName = profile.LastName;
+			existingProfile.BirthYear = profile.BirthYear;
+
+			context.SaveChanges();
+		}
+
+		public void Delete(Guid id)
+		{
+			using var context = new AppDbContext();
+			context.Database.EnsureCreated();
+
+			var existingProfile = context.Profiles.FirstOrDefault(profile => profile.Id == id);
+			if (existingProfile == null)
+			{
+				throw new ProfileNotFoundException($"Профиль с ID '{id}' не найден.");
+			}
+
+			context.Profiles.Remove(existingProfile);
+			context.SaveChanges();
+		}
+	}
+}
---
+++ TodoList/Services/TodoRepository.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using TodoList.Exceptions;
+
+namespace TodoList
+{
+	public class TodoRepository
+	{
+		public List<TodoItem> GetAll()
+		{
+			using var context = new AppDbContext();
+			context.Database.EnsureCreated();
+
+			return context.Todos
+				.AsNoTracking()
+				.OrderBy(todo => todo.Id)
+				.ToList();
+		}
+
+		public List<TodoItem> GetAllByProfile(Guid profileId)
+		{
+			using var context = new AppDbContext();
+			context.Database.EnsureCreated();
+
+			return context.Todos
+				.AsNoTracking()
+				.Where(todo => todo.ProfileId == profileId)
+				.OrderBy(todo => todo.Id)
+				.ToList();
+		}
+
+		public TodoItem? GetById(int id)
+		{
+			using var context = new AppDbContext();
+			context.Database.EnsureCreated();
+
+			return context.Todos
+				.AsNoTracking()
+				.FirstOrDefault(todo => todo.Id == id);
+		}
+
+		public void Add(TodoItem item)
+		{
+			using var context = new AppDbContext();
+			context.Database.EnsureCreated();
+
+			context.Todos.Add(item);
+			context.SaveChanges();
+		}
+
+		public void Update(TodoItem item)
+		{
+			using var context = new AppDbContext();
+			context.Database.EnsureCreated();
+
+			var existingItem = context.Todos.FirstOrDefault(todo => todo.Id == item.Id);
+			if (existingItem == null)
+			{
+				throw new TaskNotFoundException($"Задача с ID '{item.Id}' не найдена.");
+			}
+
+			existingItem.Text = item.Text;
+			existingItem.Status = item.Status;
+			existingItem.CreatedAt = item.CreatedAt;
+			existingItem.LastUpdated = item.LastUpdated;
+			existingItem.ProfileId = item.ProfileId;
+
+			context.SaveChanges();
+		}
+
+		public void Delete(int id)
+		{
+			using var context = new AppDbContext();
+			context.Database.EnsureCreated();
+
+			var existingItem = context.Todos.FirstOrDefault(todo => todo.Id == id);
+			if (existingItem == null)
+			{
+				throw new TaskNotFoundException($"Задача с ID '{id}' не найдена.");
+			}
+
+			context.Todos.Remove(existingItem);
+			context.SaveChanges();
+		}
+
+		public void SetStatus(int id, TodoStatus status)
+		{
+			using var context = new AppDbContext();
+			context.Database.EnsureCreated();
+
+			var existingItem = context.Todos.FirstOrDefault(todo => todo.Id == id);
+			if (existingItem == null)
+			{
+				throw new TaskNotFoundException($"Задача с ID '{id}' не найдена.");
+			}
+
+			existingItem.Status = status;
+			existingItem.LastUpdated = DateTime.Now;
+
+			context.SaveChanges();
+		}
+	}
+}
